90aaeba6dddb796b6448d7ca9f5b2945be49ce52
diegocardoso93 <dieg0card0s0@hotmail.com>
2020-05-12 03:27:50 -0300

    first commit

---
+++ download-dataset.js
@@ -0,0 +1,2 @@
+
+// https://www.ncbi.nlm.nih.gov/genomes/VirusVariation/vvsearch2/?fq=%7B!tag=SeqType_s%7DSeqType_s:(%22Nucleotide%22)&fq=VirusLineageId_ss:(2697049)&cmd=download&sort=SourceDB_s%20desc,CreateDate_dt%20desc,id%20asc&dlfmt=fasta&fl=id,Definition_s,Nucleotide_seq
