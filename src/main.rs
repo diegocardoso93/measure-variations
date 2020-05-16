9d5b29d6c320e09a3fbc260a55c0e48069d199a3
diegocardoso93 <dieg0card0s0@hotmail.com>
2020-05-16 14:38:08 -0300

    print_diff +_+

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate bio;
+extern crate difference;
 
 use bio::io::fasta;
 use std::str;
 use strsim::{levenshtein, sorensen_dice};
+use difference::print_diff;
 
 fn main() {
     let mut sequences = Vec::new();
@@ -14,6 +16,20 @@ fn main() {
         sequences.push(seq.to_owned());
     }
 
-    println!("{:?}", levenshtein(str::from_utf8(&sequences[0]).unwrap(), str::from_utf8(&sequences[1]).unwrap()));
-    println!("{:?}", sorensen_dice(str::from_utf8(&sequences[0]).unwrap(), str::from_utf8(&sequences[1]).unwrap()));
+    print_diff(str::from_utf8(&sequences[0]).unwrap(), str::from_utf8(&sequences[1]).unwrap(), "");
+
+    // println!(
+    //     "{:?}",
+    //     levenshtein(
+    //         str::from_utf8(&sequences[0]).unwrap(),
+    //         str::from_utf8(&sequences[1]).unwrap()
+    //     )
+    // );
+    println!(
+        "{:?}",
+        sorensen_dice(
+            str::from_utf8(&sequences[0]).unwrap(),
+            str::from_utf8(&sequences[1]).unwrap()
+        )
+    );
 }
