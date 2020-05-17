8884f72cc3637eafa522832dd5dcd4cda4fad307
diegocardoso93 <dieg0card0s0@hotmail.com>
2020-05-17 02:56:02 -0300

    add dataset
    Former-commit-id: 63964de76587333e2d40895d514f6ac6d2169b70

---
+++ split_files/src/main.rs
@@ -0,0 +1,32 @@
+extern crate bio;
+
+use bio::io::fasta;
+use std::str;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn main() {
+    let reader = fasta::Reader::from_file("sequences.fasta").unwrap();
+
+    for result in reader.records() {
+        let record = result.unwrap();
+        let seq = record.seq();
+
+        let filename = "files/".to_owned() + record.id();
+        let path = Path::new(&filename);
+        let display = path.display();
+
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("couldn't create {}: {}", display, why.description()),
+            Ok(file) => file,
+        };
+
+        match file.write_all(seq) {
+            Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
+            Ok(_) => println!("successfully wrote to {}", display),
+        }
+    }
+}
